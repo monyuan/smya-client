0793ae7f634364455ce0a31540ba504728196d11
MonYuan <im@monyuan.com>
2020-11-29 00:18:02 +0800

    del build

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import _thread
 import sys
 from time import sleep
+from os.path import abspath, dirname, join
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QColor, QIcon, QCursor
+from PyQt5.QtWidgets import (QGraphicsDropShadowEffect,
+                             QSystemTrayIcon, QMenu, QAction, QMainWindow, QApplication)
 
 from qss import style
 from ui import *
@@ -19,17 +21,24 @@ TITLE_COLOR = "#ffffff"  # 头部颜色
 from handle import Handle
 
 
+def resource_path(*relative_path):
+    base_path = getattr(sys, '_MEIPASS', abspath(join(dirname(__file__))))
+    return join(base_path, *relative_path).replace("\\", "/")
+
+print(resource_path())
+
 class MainWindow(QMainWindow, Ui_MainWindow):
-    
+
     def __init__(self):
+        super(MainWindow, self).__init__()
+        ti = TrayIcon(self)
+        ti.show()
         self.handler = Handle(self)
         self.m_flag = None
         self.m_Position = None
-        super(MainWindow, self).__init__()
         self.setupUi(self)
         self.init_ui()
         self.click_handler()
-        
         self.shadow = QGraphicsDropShadowEffect()
         self.shadow.setBlurRadius(15)
         self.shadow.setColor(QColor(0, 0, 0, 255))
@@ -42,10 +51,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.shadow2.setBlurRadius(15)
         self.shadow2.setOffset(0, 0)
         self.shadow2.setColor(QColor(0, 0, 0, 255))
-        
         self.widget_2.setGraphicsEffect(self.shadow)
         self.widget.setGraphicsEffect(self.shadow1)  # 加阴影，更立体
-    
+
     def init_ui(self):
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 去边框
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 设置窗口背景透明
@@ -53,19 +61,33 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.centralwidget.setAutoFillBackground(True)
         self.pushButton.setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT))  # 设置按钮大小
         self.pushButton_2.setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT))  # 设置按钮大小
+        btn_close_normal = resource_path(join("img", "btn_close_normal.png"))
+        btn_close_down2 = resource_path(join("img", "btn_close_down2.png"))
+        btn_close_down = resource_path(join("img", "btn_close_down.png"))
+        btn_set_normal = resource_path(join("img", "btn_set_normal.png"))
+        btn_min_normal = resource_path(join("img", "btn_min_normal.png"))
         Qss = 'QWidget#widget_2{background-color: %s;}' % BACKGROUND_COLOR
         Qss += 'QWidget#widget{background-color: %s;border-top-right-radius:5 ;border-top-left-radius:5 ;}' % TITLE_COLOR
         Qss += 'QWidget#widget_3{background-color: %s;}' % TITLE_COLOR
-        Qss += 'QPushButton#pushButton{margin-top:6;background-color: %s;border-image:url(./img/btn_close_normal.png);border-top-right-radius:5 ;}' % TITLE_COLOR
-        Qss += 'QPushButton#pushButton:hover{border-image:url(./img/btn_close_down2.png); border-top-right-radius:5 ;}'
-        Qss += 'QPushButton#pushButton:pressed{border-image:url(./img/btn_close_down.png);border-top-right-radius:5 ;}'
-        Qss += 'QPushButton#pushButton_2{margin-top:8;background-color: %s;border-image:url(./img/btn_min_normal.png);}' % TITLE_COLOR
-        Qss += 'QPushButton#pushButton_2:hover{background-color: %s;border-image:url(./img/btn_min_normal.png);}' % BACKGROUND_COLOR
+        Qss += 'QPushButton#pushButton{margin-top:6;background-color: %s;border-image:url(%s);border-top-right-radius:5 ;}' % (
+            TITLE_COLOR, btn_close_normal)
+        Qss += 'QPushButton#pushButton:hover{border-image:url(%s); border-top-right-radius:5 ;}' % btn_close_down2
+        Qss += 'QPushButton#pushButton:pressed{border-image:url(%s);border-top-right-radius:5 ;}' % btn_close_down
+        Qss += 'QPushButton#pushButton_2{margin-top:8;background-color: %s;border-image:url(%s);}' % (
+            TITLE_COLOR, btn_min_normal)
+        Qss += 'QPushButton#pushButton_2:hover{background-color: %s;border-image:url(%s);}' % (BACKGROUND_COLOR,
+                                                                                               btn_min_normal)
         Qss += 'QPushButton#pushButton_2:pressed{background-color: %s;border-top-left-radius:5 ;}' % BACKGROUND_COLOR
-        Qss += 'QPushButton#pushButton_3{background-color: %s;border-top-left-radius:5 ;border:0;}' % TITLE_COLOR
+        Qss += 'QPushButton#pushButton_3{border-image:url(%s);background-color: %s;border-top-left-radius:5}' % (
+            btn_set_normal, TITLE_COLOR)
         Qss += '#label{background-color:rbga(0,0,0,0);color:#111111;}'
         self.setStyleSheet(Qss)  # 边框部分qss重载
-    
+        self.setWindowIcon(QIcon(resource_path("icon.ico")))
+        self.setWindowTitle('神秘鸭')
+        self.textBrowser.append("欢迎使用神秘鸭 smya.cn")
+        _thread.start_new_thread(self.handler.ad, ())
+        self.handler.app_update()
+
     def click_handler(self):
         """
         按钮点击事件
@@ -74,20 +96,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.pushButton.clicked.connect(self.close)  # 关闭
         self.pushButton_2.clicked.connect(self.ButtonMinSlot)  # 最小化
         self.login.clicked.connect(self.handler.login)
+        self.ad1.mousePressEvent = self.handler.jump_ad
         _thread.start_new_thread(self.info_window_scroll, ())
-    
+
+    def send_key_event(self, data):
+        self.show()
+
     def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
             self.m_flag = True
             self.m_Position = event.globalPos() - self.pos()  # 获取鼠标相对窗口的位置
             event.accept()
             self.setCursor(QCursor(QtCore.Qt.OpenHandCursor))
-    
+
     def mouseReleaseEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
             self.m_flag = False
             self.setCursor(QCursor(QtCore.Qt.ArrowCursor))
-    
+
     def mouseMoveEvent(self, event):
         """
         拖动事件
@@ -97,14 +123,63 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if QtCore.Qt.LeftButton and self.m_flag:
             self.move(event.globalPos() - self.m_Position)  # 更改窗口位置
             event.accept()
-    
+
+    def keyPressEvent(self, QKeyEvent):
+        if QKeyEvent.key() == QtCore.Qt.Key_Escape:
+            self.hide()
+
     def ButtonMinSlot(self):
-        self.showMinimized()
-    
+        self.hide()
+
     def info_window_scroll(self):
         while True:
             self.textBrowser.moveCursor(self.textBrowser.textCursor().End)
             sleep(0.5)
+
+
+class TrayIcon(QSystemTrayIcon):
+    def __init__(self, parent=None):
+        super(TrayIcon, self).__init__(parent)
+        self.showMenu()
+        self.other()
+
+    def showMenu(self):
+        "设计托盘的菜单，这里我实现了一个二级菜单"
+        self.menu = QMenu()
+        self.menu1 = QMenu()
+        self.showAction1 = QAction("显示窗口", self, triggered=self.showM)
+        self.quitAction = QAction("退出程序", self, triggered=self.quit)
+
+        self.menu1.addAction(self.showAction1)
+
+        self.menu.addAction(self.showAction1)
+        self.menu.addAction(self.quitAction)
+        self.setContextMenu(self.menu)
+
+    def other(self):
+        self.activated.connect(self.iconClied)
+        self.setIcon(QIcon("icon.ico"))
+        self.icon = self.MessageIcon()
+        # 设置图标
+
+    def iconClied(self, reason):
+        "鼠标点击icon传递的信号会带有一个整形的值，1是表示单击右键，2是双击，3是单击左键，4是用鼠标中键点击"
+        if reason == 2 or reason == 3:
+            pw = self.parent()
+            if pw.isVisible():
+                pw.hide()
+            else:
+                pw.show()
+
+    def showM(self):
+        pw = self.parent()
+        pw.show()
+
+    def quit(self):
+        "保险起见，为了完整的退出"
+        self.setVisible(False)
+        self.parent().exit()
+        sys.exit()
 
 
 if __name__ == '__main__':
@@ -113,3 +188,6 @@ if __name__ == '__main__':
     app.setStyleSheet(style())
     w.show()
     sys.exit(app.exec_())
+
+# build
+# pyinstaller --clean -F -w app.py -i icon.ico --add-data "img;img" --add-data "icon.ico;./"
--- handle.py
+++ handle.py
@@ -1,30 +1,37 @@
 import _thread
 import binascii
 import json
+import sys
 from datetime import datetime
 from time import sleep
-
+import os
+import re
+import subprocess
 import paho.mqtt.client as mqtt
 import requests
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QImage, QPixmap, QDesktopServices
 from PyQt5.QtWidgets import QMessageBox
 from pyDes import des, CBC, PAD_PKCS5
 
-SERVER = "http://127.0.0.1:7777/client/login"
+SERVER = "http://127.0.0.1:7777"
+SERVER_VERSION = 1
+SERVER_TAG = 1
 
 
 def log_info(text):
     time = datetime.strftime(datetime.now(), '%H:%M:%S')
-    return '<p>{} {}</p>'.format(time, text)
+    return '<span>{} {}</span>'.format(time, text)
 
 
 def log_success(text):
     time = datetime.strftime(datetime.now(), '%H:%M:%S')
-    return '<p style="color: green">{} {}</p>'.format(time, text)
+    return '<span style="color: green">{} {}</span>'.format(time, text)
 
 
 def log_error(text):
     time = datetime.strftime(datetime.now(), '%H:%M:%S')
-    return '<p style="color: red">{} {}</p>'.format(time, text)
+    return '<span style="color: red">{} {}</span>'.format(time, text)
 
 
 def encrypt(s, k):
@@ -34,7 +41,9 @@ def encrypt(s, k):
     :param k: 你的密钥：你的设备安全码加上当前时(16:00),eg:30233316
     :return: 加密后的字符串
     """
-    k = k + str(datetime.now().hour)
+    current_hour = str(datetime.now().hour)
+    if len(current_hour) == 1: current_hour += "0"
+    k = str(k) + current_hour
     k = des(k, CBC, k, pad=None, padmode=PAD_PKCS5)
     en = k.encrypt(s, padmode=PAD_PKCS5)
     return str(binascii.b2a_hex(en), encoding="utf8")
@@ -42,7 +51,9 @@ def encrypt(s, k):
 
 def decrypt(s, k):
     """解密"""
-    iv = str(k) + str(datetime.now().hour)
+    current_hour = str(datetime.now().hour)
+    if len(current_hour) == 1: current_hour += "0"
+    iv = str(k) + current_hour
     k = des(iv, CBC, iv, pad=None, padmode=PAD_PKCS5)
     s = bytes(s, encoding="utf8")
     de = k.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
@@ -60,7 +71,7 @@ def login_to_server(device_id, safe_code):
         "device_id": device_id,
         "str": encrypt(safe_code + device_id, safe_code)
     }
-    res = requests.post(SERVER, data=json.dumps(data), headers=headers).json()
+    res = requests.post(SERVER + "/client/login", data=json.dumps(data), headers=headers).json()
     return res
 
 
@@ -74,7 +85,48 @@ class Handle(object):
         self.connect_message = 0
         self.client = None
         self.safe_code = 0
-    
+        self.jump = SERVER
+
+    def ad(self):
+        res = requests.get(SERVER + "/server_ad").json()
+        self.jump = res['data']['url']
+        img_url = requests.get(res['data']['image'])
+        img = QImage.fromData(img_url.content)
+        self.w.ad1.setPixmap(QPixmap.fromImage(img))
+        self.w.ad1.setOpenExternalLinks(True)
+
+    def jump_ad(self, data):
+        QDesktopServices.openUrl(QUrl(self.jump))
+
+    def app_update(self):
+        """
+        检测是否有新版本，有就升级，不然后导致无法使用,无论如何请保留此功能
+        重大BUG、安全风险等都会进行升级
+        :return:
+        """
+        res = requests.get(SERVER + "/app_update?server_tag=%s" % SERVER_TAG).json()
+        if res['code'] != 0: return
+        server_version = res['data']['version']
+        update_url = res['data']['update_url']
+        update_type = res['data']['type']
+        update_message = res['data']['message']
+        if int(server_version) > int(SERVER_VERSION):  # 可升级
+            if update_type == 1:  # 不向下兼容，必须要升级
+                QMessageBox.information(self.w, "发现新版本！版本号：{}".format(server_version), "{}".format(update_message),
+                                        QMessageBox.Ok)
+                QDesktopServices.openUrl(QUrl(update_url))
+                sys.exit()
+            else:  # 可选是否升级
+                reply = QMessageBox.information(self.w, "发现新版本！", "{}".format(update_message),
+                                                QMessageBox.Ok, QMessageBox.No)
+                if reply == QMessageBox.Ok:
+                    QDesktopServices.openUrl(QUrl(update_url))
+                    sys.exit()
+                else:
+                    # 如果忽略升级
+                    pass
+        return
+
     def login(self):
         """
         登陆到服务器
@@ -82,57 +134,57 @@ class Handle(object):
         """
         device_id = self.w.device_id.text().strip()
         self.safe_code = self.w.safe_code.text().strip()
-        self.w.login.setDisabled(True)
-        self.w.device_id.setDisabled(True)
-        self.w.safe_code.setDisabled(True)
         if len(device_id) != 32:
-            QMessageBox.warning(self.w, "错误！", "你的设备ID错误！")
+            QMessageBox.critical(self.w, "错误！", "你的设备ID错误！")
             return
         if len(self.safe_code) != 6:
-            QMessageBox.warning(self.w, "错误！", "你的安全码错误！")
+            QMessageBox.critical(self.w, "错误！", "你的安全码错误！")
             return
-        
+
         try:
+            self.input_status(True)
             self.connect_message = 0
             self.w.textBrowser.append(log_info("开始连接服务器..."))
             res = login_to_server(device_id, self.safe_code)
             if res["code"] == 0:
                 server = res["data"]["server"]  # this is subscribe server addr
                 subscribe = res["data"]["subscribe"]  # this is subscribe topic
-                print(subscribe)
                 self.subscribe = subscribe
                 self.server = server
                 self.device_id = device_id
                 _thread.start_new_thread(self.connect_server, ())
             else:
-                QMessageBox.warning(self.w, "错误！", "{}".format(res["msg"]))
+                self.w.textBrowser.append(log_error(res["msg"]))
+                self.input_status(False)
         except Exception as E:
             print(E)
-            self.w.login.setDisabled(False)
-            self.w.device_id.setDisabled(False)
-            self.w.safe_code.setDisabled(False)
+            self.input_status(False)
             self.w.textBrowser.append(log_error("连接异常，请检查网络后重试！"))
-    
+
     def mqtt_on_connect(self, client, userdata, flags, rc):
+        """与服务端建立连接"""
         if rc == 0:
-            print(self.subscribe)
             client.subscribe('client/{}'.format(self.subscribe), qos=1)
             self.w.textBrowser.append(log_success("连接成功，初始化..."))
             sleep(2)
             self.w.textBrowser.append(log_success("初始化完成！"))
-        
         else:
             self.connect_message += 1
             self.w.textBrowser.append(log_error("掉线重连，第{}次...".format(self.connect_message)))
-    
+
     def mqtt_on_message(self, client, userdata, msg):
+        """服务端下发指令"""
         self.w.textBrowser.append(log_info("服务端下发了指令..."))
         try:
             en_data = str(msg.payload, encoding="utf-8")
-            de_data = decrypt(en_data + "444", self.safe_code)
+            de_data = decrypt(en_data, self.safe_code)
+            json_data = json.loads(de_data)
+            command_name = json_data['command_name']
+            self.w.textBrowser.append(log_info("执行[{}]指令...".format(command_name)))
+            Execute(self.w, json_data['command'], json_data['type']).do()
         except:
             self.w.textBrowser.append(log_error("指令解密失败！"))
-    
+
     def connect_server(self):
         """
         client_id -  你可以自定义
@@ -144,3 +196,82 @@ class Handle(object):
         self.client.on_message = self.mqtt_on_message
         self.client.connect(self.server, self.server_port, 60)
         self.client.loop_forever()
+
+    def input_status(self, status):
+        """
+        按钮及输入框的状态
+        :param status:
+        :return:
+        """
+        self.w.login.setDisabled(status)
+        self.w.device_id.setDisabled(status)
+        self.w.safe_code.setDisabled(status)
+
+
+class Execute:
+    def __init__(self, w, command, command_type):
+        self.w = w
+        self.command = command
+        self.type = command_type
+
+    def do(self):
+        """执行脚本"""
+        dic = {1: self.open_any,
+               2: self.del_any,
+               3: self.add_any,
+               4: self.kill_any,
+               5: self.run_any,
+               }
+        dic[self.type]()
+
+    def open_any(self):
+        """打开文件或程序"""
+        try:
+            os.startfile(self.command)
+            self.w.textBrowser.append(log_success("执行成功！"))
+        except Exception as E:
+            self.w.textBrowser.append(log_error("执行失败：{}".format(E)))
+
+    def del_any(self):
+        """删除文件"""
+        try:
+            if os.path.isdir(self.command):
+                os.rmdir(self.command)
+            if os.path.isfile(self.command):
+                os.remove(self.command)
+            self.w.textBrowser.append(log_success("执行成功！"))
+        except Exception as E:
+            self.w.textBrowser.append(log_error("执行失败：{}".format(E)))
+
+    def add_any(self):
+        """创建文件"""
+        try:
+            try:
+                file = re.findall(r'\.[^.\\/:*?"<>|\r\n]+$', self.command)
+                n = file[0]
+                with open(self.command, "w") as f:
+                    f.write("")
+            except:
+                os.mkdir(self.command)
+            self.w.textBrowser.append(log_success("执行成功！"))
+        except Exception as E:
+            self.w.textBrowser.append(log_error("执行失败：{}".format(E)))
+
+    def kill_any(self):
+        """结束进程"""
+        try:
+            if os.sep == "/":
+                subprocess.Popen("pkill -9 {}".format(self.command), close_fds=True)
+            else:
+                subprocess.Popen("taskkill /f /im {}".format(self.command), close_fds=True)
+            self.w.textBrowser.append(log_success("执行成功！"))
+        except Exception as E:
+            self.w.textBrowser.append(log_error("执行失败：{}".format(E)))
+
+    def run_any(self):
+        """执行任意脚本"""
+        try:
+            subprocess.Popen("{}".format(self.command), close_fds=True)
+            self.w.textBrowser.append(log_success("执行成功！"))
+        except Exception as E:
+            self.w.textBrowser.append(log_error("执行失败：{}".format(E)))
